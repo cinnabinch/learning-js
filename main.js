d10bff601174a3e43ac9465326657e27c6ad2da2
cinnabinch <91610140+cinnabinch@users.noreply.github.com>
2021-11-29 13:25:13 -0600

    added console.log, document.write, and alert to js page

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-document.querySelectorAll('.btn').forEach((item) => {
-    item.addEventListener('click',  )
-}
+document.querySelectorAll('.btn').forEach(function (item) {
+        item.addEventListener('click', () => {
+            alert('Manny is the best');
+        });
+    });
+
+
+    // Helper Methods
+
+alert('Hello abby');
+document.write("hello abby");
+console.log('hello abby from console');
+
+alert({name: 'Abby', email:'abby@abby.com'});
+document.write({name: 'Abby', email:'abby@abby.com'});
+console.log({name: 'Abby', email:'abby@abby.com'});
